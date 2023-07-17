74750f89ebf4b4f5feb62fe1d13647469ffb57d7
joeljose9207 <94070079+joeljose9207@users.noreply.github.com>
2023-07-18 00:20:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,3 +19,17 @@ quote_by_category = pyquotegen.get_quote("inspirational")
 def send_welcome(message):
 	 # bot.send_message(message.chat.id,message.text)
 	   bot.send_message(message.chat.id,quote_by_category)
+	   bot.send_message("helo")
+#Handle The '/ask'
+@bot.message_handler(commands=['ask'])
+def first_process(message):
+	bot.send_message(message.chat.id,"Send Me your Question")
+	bot.register_next_step_handler(message,second_process)
+def again_send(message):
+  bot.register_next_step_handler(message,second_process)
+def second_process(message):
+  bot.send_message(message.chat.id,get_response(message.text))
+  again_send(message)
+
+ 
+bot.infinity_polling()
