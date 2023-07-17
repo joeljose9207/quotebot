afc6198ca0c7e4f276775689cfaceabf63f8822e
joeljose9207 <94070079+joeljose9207@users.noreply.github.com>
2023-07-18 00:11:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,15 @@ import pyquotegen
 
 # Get a random quote
 quote = pyquotegen.get_quote()
-print(quote)
+
 
 # Get a quote by specific category
 quote_by_category = pyquotegen.get_quote("inspirational")
-print(quote_by_category)
+
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+	 # bot.send_message(message.chat.id,message.text)
+	   bot.send_message(message.chat.id,quote_by_category)
